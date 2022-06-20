2617fb1eb018dacd3d8e926b4393f0f9d28e8a39
amishasubedi <amisha.subedi1@gmail.com>
2022-06-20 17:23:51 -0500

    displayed food items with their description

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Fragment } from "react";
+
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 import "./App.css";
-import React, { Fragment } from "react";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from "react";
-import mealsImage from "../../assets.meals.jpg";
+
+import HeaderCartButton from "./HeaderCartButton";
+import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 
 const Header = (props) => {
@@ -7,7 +9,7 @@ const Header = (props) => {
     <React.Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes[".main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
