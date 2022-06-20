13385c62ce02d1adb82d44c53e97d0a8cd1d8c4f
amishasubedi <amisha.subedi1@gmail.com>
2022-06-20 14:14:16 -0500

    first-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Layout/Header";
+import "./App.css";
+import React, { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
