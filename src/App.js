84468558b2dd927386a508ea63312ed325cd184c
amishasubedi <amisha.subedi1@gmail.com>
2022-06-21 23:13:35 -0500

    added a form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Fragment } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
-import "./App.css";
 
 function App() {
   return (
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>React Meals</h1>
+        <h1>ReactMeals</h1>
         <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
