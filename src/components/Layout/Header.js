b31d8e3939da873e572eb8f566f767c0bfe55a6f
amishasubedi <amisha.subedi1@gmail.com>
2022-06-21 22:11:33 -0500

    displayed individual meal items

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import { Fragment } from "react";
 
 import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.jpg";
@@ -6,15 +6,15 @@ import classes from "./Header.module.css";
 
 const Header = (props) => {
   return (
-    <React.Fragment>
+    <Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
         <HeaderCartButton />
       </header>
-      <div className={classes[".main-image"]}>
+      <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
-    </React.Fragment>
+    </Fragment>
   );
 };
 
